9bd0cdcc2b745cb7785477497585640db369cc78
“Nader <nadmot23@gmail.com>
2024-02-19 20:02:25 +0000

    Added event listener to photo URL #1

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
+'use strict';
 // /* global data */
 const $urlLinkInput = document.querySelector('#photo-url');
 if (!$urlLinkInput) throw new Error('$urlLinkInput query failed');
-
-$urlLinkInput.addEventListener('input', (event: Event) => {
+$urlLinkInput.addEventListener('input', (event) => {
   $urlLinkInput.addEventListener('blur', () => {
-    const eventTarget = event.target as HTMLInputElement;
+    let eventTarget = event.target;
     if (eventTarget.value.match(/\.(jpeg|jpg|gif|png)$/)) {
-      const $previewPhoto = document.querySelector('.preview');
+      let $previewPhoto = document.querySelector('.preview');
       $previewPhoto?.setAttribute('src', eventTarget.value);
     }
   });
